1291b46f29f9f63edacebd7b6c0fd5e18a86aa9c
Schnecke <eiweala@live.com>
2021-01-07 21:30:22 +0800

    folder structured properly

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+//my change is a comment
 function App() {
   return (
     <div className="App">
