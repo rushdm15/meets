0b6b4c8a644eeef3725c09f4745b2680c55fd40a
Doug Rush <71516868+rushdm15@users.noreply.github.com>
2021-01-09 14:45:14 +0200

    Create mockData

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import CitySearch from '../CitySearch';
+import { mockData } from "../mock-data";
 
 describe('<CitySearch /> component', () => {
   test('render text input', () => {
