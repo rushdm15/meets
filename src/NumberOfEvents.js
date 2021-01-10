a62d491d6ae403661520b9fdeeecbdb74bed95ba
Doug Rush <71516868+rushdm15@users.noreply.github.com>
2021-01-10 15:53:24 +0200

    Created NumberOfEvents.js

---
+++ src/NumberOfEvents.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class NumberOfEvents extends Component {
+  render() {
+    return (
+      <div className="NumberOfEvents">
+         <input
+            type="text"
+            className="city"
+         />    
+      </div>
+    );
+  }
+}
+
+export default NumberOfEvents;
