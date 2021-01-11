1bb2405ab7da49255989e0906796e0c098ae57c6
Doug Rush <71516868+rushdm15@users.noreply.github.com>
2021-01-11 14:34:22 +0200

    imports

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-// import CitySearch from '../CitySearch';
+import CitySearch from '../CitySearch';
 import NumberOfEvents from '../NumberOfEvents';
+import { mockData } from "../mock-data";
+import { extractLocations } from "../api";
+
 
 describe('<NumberOfEvents /> component', () => {
     test('render text input', () => {
       const NumberOfEventsWrapper = shallow(<NumberOfEvents />);
       expect(NumberOfEventsWrapper.find('.city')).toHaveLength(1);
     });
+    const eventObject = { target: { value: 'Berlin' }};
+    NumberOfEventsWrapper.find('.city').simulate('change', eventObject);
+    expect(NumberOfEventsWrapper.state('query')).toBe('Berlin');
   });
+
+  test('render list of suggestions correctly', () => {
+    const locations = extractLocations(mockData);
+    NumberOfEventsWrapper.setState({ suggestions: locations });
+    const suggestions = NumberOfEventsWrapper.state('suggestions');
+    expect(NumberOfEventsWrapper.find('.suggestions li')).toHaveLength(suggestions.length + 1);
+    for (let i = 0; i < suggestions.length; i += 1) {
+      expect(NumberOfEventsWrapper.find('.suggestions li').at(i).text()).toBe(suggestions[i]);
+    }
+  });
+
+});
