48ce186a9b3531150740d93bcbd6be6062c6640e
Doug Rush <71516868+rushdm15@users.noreply.github.com>
2021-01-12 15:22:20 +0200

    handleInputChanged/Clicked

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,13 +1,47 @@
 import React, { Component } from 'react';
 
 class NumberOfEvents extends Component {
+  state = {
+    query: '',
+    suggestions: []
+}
+
+handleInputChanged = (event) => {
+    const value = event.target.value;
+    const suggestions = this.props.locations.filter((location) => {
+        return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+    });
+    this.setState({ 
+      query: value,
+      suggestions,
+    });
+  }
+
+  handleItemClicked = (suggestion) => {
+    this.setState({
+      query: suggestion
+    });
+  }
+
   render() {
     return (
       <div className="NumberOfEvents">
-         <input
-            type="text"
-            className="city"
-         />    
+        <input
+          type="text"
+          className="city"
+          value={this.state.query}
+          onChange={this.handleInputChanged}
+        />
+      <ul className="suggestions">
+        {this.state.suggestions.map((suggestion) => (
+          <li 
+            key={suggestion}
+            onClick={() => this.handleItemClicked(suggestion)}>{suggestion}</li>
+        ))}
+        <li> 
+          <b>See all cities</b>
+        </li>
+      </ul>
       </div>
     );
   }
