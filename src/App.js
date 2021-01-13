f50ba86df0f7a631603fba52e00d9a7d6180737a
Doug Rush <71516868+rushdm15@users.noreply.github.com>
2021-01-13 20:03:56 +0200

    locations test

--- src/App.js
+++ src/App.js
@@ -6,13 +6,14 @@ import NumberOfEvents from './NumberOfEvents';
 
 class App extends Component {
   state = {
-    events: []
+    events: [],
+    locations: []
   }
   render() {
     return (
       <div className="App">
-        <CitySearch />
-        <EventList />
+        <CitySearch locations={this.state.locations} />
+        <EventList events={this.state.events} />
         <NumberOfEvents />
       </div>
     );
