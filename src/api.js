6a6f33e78859fe14eaac634eebea5d4e8a363b8c
Doug Rush <71516868+rushdm15@users.noreply.github.com>
2021-01-13 21:00:36 +0200

    export const getEvents

--- src/api.js
+++ src/api.js
@@ -11,3 +11,7 @@ export const extractLocations = (events) => {
     var locations = [...new Set(extractLocations)];
     return locations;
   };
+
+  export const getEvents = async () => {
+    return mockData;
+  };
