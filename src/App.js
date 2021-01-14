3a53d5832b86bae17590dbbac8f844dffc1d2a8d
Doug Rush <71516868+rushdm15@users.noreply.github.com>
2021-01-14 11:29:39 +0200

    extractLocations

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
-import { getEvents } from './api';
+import { getEvents, extractLocations } from './api';
 
 class App extends Component {
   state = {
