7562af0f83dfb433b8461ec0c3b039aad5fb5a11
Doug Rush <71516868+rushdm15@users.noreply.github.com>
2021-01-14 16:55:24 +0200

    Access Token Found in localStorage

--- src/api.js
+++ src/api.js
@@ -23,6 +23,17 @@ if (!accessToken || tokenCheck.error) {
   return code && getToken(code);
 }
 return accessToken;
+
+const checkToken = async (accessToken) => {
+  const result = await fetch(
+    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+  )
+    .then((res) => res.json())
+    .catch((error) => error.json());
+
+  return result;
+};
+
 export const extractLocations = (events) => {
     var extractLocations = events.map((event) => event.location);
     var locations = [...new Set(extractLocations)];
@@ -30,11 +41,37 @@ export const extractLocations = (events) => {
   };
 
 export const getEvents = async () => {
-    return mockData;
-  };
+  if (window.location.href.startsWith('http://localhost')
+  ) {  
+  return mockData;
+  }
+}
 
 export const getAccessToken = async () => {
   const accessToken = localStorage.getItem('access_token');
 }  
 
-}  
+export const getEvents = async () => {
+  NProgress.start();
+
+  if (window.location.href.startsWith("http://localhost")) {
+    NProgress.done();
+    return mockData;
+  }
+
+
+  const token = await getAccessToken();
+
+  if (token) {
+    removeQuery();
+    const url = 'YOUR_GET_EVENTS_API_ENDPOINT' + '/' + token;
+    const result = await axios.get(url);
+    if (result.data) {
+      var locations = extractLocations(result.data.events);
+      localStorage.setItem("lastEvents", JSON.stringify(result.data));
+      localStorage.setItem("locations", JSON.stringify(locations));
+    }
+    NProgress.done();
+    return result.data.events;
+  }
+};
