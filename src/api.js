2f9b8a236767633aa3b2cc7389aba324c6d0dfab
Doug Rush <71516868+rushdm15@users.noreply.github.com>
2021-01-15 12:23:53 +0200

    changed order

--- src/api.js
+++ src/api.js
@@ -8,6 +8,45 @@ import NProgress from 'nprogress';
  * It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
  * The Set will remove all duplicates from the array.
  */
+const removeQuery = () => {
+  if (window.history.pushState && window.location.pathname) {
+    var newurl =
+      window.location.protocol +
+      "//" +
+      window.location.host +
+      window.location.pathname;
+    window.history.pushState("", "", newurl);
+  } else {
+    newurl = window.location.protocol + "//" + window.location.host;
+    window.history.pushState("", "", newurl);
+  }
+};
+
+const getToken = async (code) => {
+  const encodeCode = encodeURIComponent(code);
+  const { access_token } = await fetch(
+    'https://jpo6r3bj30.execute-api.eu-central-1.amazonaws.com/dev/api/token' + '/' + encodeCode
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .catch((error) => error);
+
+  access_token && localStorage.setItem("access_token", access_token);
+
+  return access_token;
+};
+
+export const extractLocations = (events) => {
+    var extractLocations = events.map((event) => event.location);
+    var locations = [...new Set(extractLocations)];
+    return locations;
+  };
+
+export const getAccessToken = async () => {
+  const accessToken = localStorage.getItem('access_token');
+}  
+
 const tokenCheck = accessToken && (await checkToken(accessToken));
 
 if (!accessToken || tokenCheck.error) {
@@ -35,23 +74,6 @@ const checkToken = async (accessToken) => {
   return result;
 };
 
-export const extractLocations = (events) => {
-    var extractLocations = events.map((event) => event.location);
-    var locations = [...new Set(extractLocations)];
-    return locations;
-  };
-
-export const getEvents = async () => {
-  if (window.location.href.startsWith('http://localhost')
-  ) {  
-  return mockData;
-  }
-}
-
-export const getAccessToken = async () => {
-  const accessToken = localStorage.getItem('access_token');
-}  
-
 export const getEvents = async () => {
   NProgress.start();
 
@@ -59,7 +81,6 @@ export const getEvents = async () => {
     NProgress.done();
     return mockData;
   }
-
 
   const token = await getAccessToken();
 
