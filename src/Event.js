f031df152cb2a6a0fe2cd3831abada713d512fc6
Doug Rush <71516868+rushdm15@users.noreply.github.com>
2021-01-16 16:41:56 +0200

    state, handleShowDetails, buttons

--- src/Event.js
+++ src/Event.js
@@ -1,18 +1,40 @@
+// import { findAllByTestId } from "@testing-library/react";
 import React, { Component } from "react";
-// import EventList from './EventList';
 
 class Event extends Component {
+  state = {
+    showDetails: false
+  };
+
+  handleShowDetails = () => {
+    if(this.state.showDetails === false) {
+      this.setState({showDetails: true});
+    }
+    else {
+      this.setState({showDetails: false});
+    }
+  }
+
   render() {
-    const { events } = this.props;
+    const { event } = this.props;
+    const showDetails = this.state.showDetails;
+
     return (
-      // <div>{Event.summary}</div>
-      <ul className="Event">
-          {events.map(event =>
-         <li key={event.id}>
-          <Event event={event} />
-        </li>
+      <div className="event">
+        <div className="event_wrapper">
+          <p className="event_name">{event.summary}</p>
+          <p className="event_location">{event.start.dateTime}</p>
+
+          <button onClick={() => this.handleShowDetails()}>
+            {showDetails ? "Hide Details" : "Show Details"}
+          </button>
+        </div>
+        {showDetails && (
+          <div className="event_details">
+            <p className="event_description">{event.description}</p>
+          </div>
         )}
-      </ul>
+      </div>
     );
   }
 }
