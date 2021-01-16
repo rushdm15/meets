ec49676dd33921826566a4aae29aedbcc816fd49
Doug Rush <71516868+rushdm15@users.noreply.github.com>
2021-01-16 21:11:41 +0200

    EndToEnd tests

---
+++ test/EndToEnd.test.js
@@ -0,0 +1,29 @@
+import puppeteer from 'puppeteer';
+
+describe('show/hide an event details', () => {
+    test('An event element is collapsed by default', async () => {
+        const browser = await puppeteer.launch();
+    
+        const page = await browser.newPage();
+        await page.goto('http://localhost:3000/');
+    
+        await page.waitForSelector('.event');
+    
+        const eventDetails = await page.$('.event .event__Details');
+        expect(eventDetails).toBeNull();
+        browser.close();
+      });    
+
+    test('User can expand an event to see its details', async () => {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto('http://localhost:3000/');
+    
+        await page.waitForSelector('.event');
+        await page.click('.event .details-btn');
+    
+        const eventDetails = await page.$('.event .event__Details');
+        expect(eventDetails).toBeDefined();
+        browser.close();
+      });
+});
