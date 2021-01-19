c723d1a523ebc00167459699490a8b6dad998447
Doug Rush <71516868+rushdm15@users.noreply.github.com>
2021-01-19 12:12:40 +0200

    import CitySearch, shallow

--- src/features/filterEventsByCity.test.js
+++ src/features/filterEventsByCity.test.js
@@ -1,9 +1,10 @@
 import { loadFeature, defineFeature } from 'jest-cucumber';
 import React from 'react';
-import { mount } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import App from '../App';
 import { mockData } from '../mock-data';
 import { extractLocations } from '../api';
+import CitySearch from '../CitySearch';
 
 const feature = loadFeature('./src/features/filterEventsByCity.feature');
 
