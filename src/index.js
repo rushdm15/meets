50f935cb5b931267a037a78e38e57e58b6d659b3
Doug Rush <71516868+rushdm15@users.noreply.github.com>
2021-01-19 21:32:44 +0200

    installed atatus

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
+import * as atatus from 'atatus-js';
 
 ReactDOM.render(
   <React.StrictMode>
