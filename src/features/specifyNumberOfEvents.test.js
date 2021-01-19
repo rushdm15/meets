9bdc20bdd9b5800b17d619945efabe19620b9ad4
Doug Rush <71516868+rushdm15@users.noreply.github.com>
2021-01-19 11:58:26 +0200

    test functions

--- src/features/specifyNumberOfEvents.test.js
+++ src/features/specifyNumberOfEvents.test.js
@@ -12,20 +12,32 @@ defineFeature(feature, test => {
       given('that a user hasnt specified the number of events per page', () => {
   
       });
+      let AppWrapper;
       when('the user searches for events', () => {
+        AppWrapper = mount(<App />);
       });
   
       then('32 event elements will display on the page', () => {
+        AppWrapper.update();
+        expect(AppWrapper.find('.event')).toBeLessThanOreEqual(32);
       });
     });
   
     test('User can change the number of events they want to see', ({ given, when, then }) => {
+      let AppWrapper;
+      given('the user wants to specify the number of events', () => {
+        AppWrapper = mount(<App />);
       });
   
       when('the user clicks on the filter option', () => {
+        const NumberOfEvents = {target: {value: 10}};  
+        AppWrapper.find('.NumberOfEvents').simulate('change', NumberOfEvents);
       });
   
       then('there should be a dial button to limit the number of events', () => {
+        const NumberOfEventsWrapper = AppWrapper.find(NumberOfEvents);
+        NumberOfEventsWrapper.setState({NumberOfEvents: 10});
+        expect(NumberOfEventsWrapper.state('NumberOfEvents')).toBe('10');
       });
     });
   });
