09fa6bb0cc00332a840e8ebc3d1722f0f4303868
Doug Rush <71516868+rushdm15@users.noreply.github.com>
2021-01-20 16:42:18 +0200

    reportWebVitals();

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ ReactDOM.render(<App />, document.getElementById('root')
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+reportWebVitals();
 
 serviceWorker.unregister();
 atatus.config('19d30c26c1ae42f5a22f55b72dc63c64').install();
