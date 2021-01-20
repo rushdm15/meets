be128001edc1c2490b063350d9b2bb4502ec0414
Doug Rush <71516868+rushdm15@users.noreply.github.com>
2021-01-20 21:16:16 +0200

    class ErrorAlert extends Component {

--- src/Alert.js
+++ src/Alert.js
@@ -57,3 +57,24 @@ class InfoAlert extends Alert {
      }
   }
 
+class ErrorAlert extends Component {
+    constructor(props) {
+      super(props);
+      this.color = 'red';
+    }
+  
+    getStyle = () => {
+      return {
+        color: this.color,
+      };
+    }
+  
+    render() {
+      return (
+        <div className="Alert">
+          <p style={this.getStyle()}>{this.props.text}</p>
+        </div>
+      );
+    }
+  }  
+
