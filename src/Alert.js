0d6f8ee82c48d1b02d5af30482604abdc0a873eb
Doug Rush <71516868+rushdm15@users.noreply.github.com>
2021-01-20 16:39:35 +0200

    class InfoAlert extends Alert {

--- src/Alert.js
+++ src/Alert.js
@@ -21,3 +21,19 @@ class Alert extends Component {
   }
 }
 
+class InfoAlert extends Alert {
+    constructor(props) {
+      super(props);
+      this.color = 'blue';
+    }
+
+    render() {
+        return (
+            <div className="CitySearch">
+            <InfoAlert text={this.state.infoText} />
+            ...
+            </div>
+        );
+        }
+  }
+
