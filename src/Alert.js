d13a004d965ee6ddd73e32a2fcbe0d78e40aed4f
Doug Rush <71516868+rushdm15@users.noreply.github.com>
2021-01-20 16:39:45 +0200

    handleInputChanged

--- src/Alert.js
+++ src/Alert.js
@@ -37,3 +37,22 @@ class InfoAlert extends Alert {
         }
   }
 
+handleInputChanged = (event) => {
+const value = event.target.value;
+this.setState({showSuggestions:true});
+const suggestions = this.props.locations.filter((location) => {
+    return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+});
+if (suggestions.length === 0) {
+    this.setState({
+    query: value,
+    infoText: 'We can not find the city you are looking for. Please try another city',
+    });
+} else {
+    return this.setState({
+    query: value,
+    suggestions,
+    infoText:''
+    });
+}
+};
