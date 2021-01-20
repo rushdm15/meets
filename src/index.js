e1f9685e72a5d54f23198763f7345ec06a3096ce
Doug Rush <71516868+rushdm15@users.noreply.github.com>
2021-01-20 16:42:23 +0200

    typo

--- src/index.js
+++ src/index.js
@@ -17,4 +17,4 @@ reportWebVitals();
 serviceWorker.unregister();
 atatus.config('19d30c26c1ae42f5a22f55b72dc63c64').install();
 
-atatus.notify(newError('Test Atatus Setup'));
+atatus.notify(new Error('Test Atatus Setup'));
