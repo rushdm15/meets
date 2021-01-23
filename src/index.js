331b2e52dfed8c3e1a6df8e4341ee573457323a8
Doug Rush <71516868+rushdm15@users.noreply.github.com>
2021-01-23 11:53:23 +0200

    <React.StrictMode>

--- src/index.js
+++ src/index.js
@@ -6,8 +6,12 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import * as atatus from 'atatus-spa';
 
-ReactDOM.render(<App />, document.getElementById('root')
-);
+ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );  
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
