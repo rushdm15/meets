1127d1be38f0476fae1605ce5d9832fb28bfb089
Doug Rush <71516868+rushdm15@users.noreply.github.com>
2021-01-24 17:05:56 +0200

    ScatterChart

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import NumberOfEvents from './NumberOfEvents';
 import { getEvents, extractLocations } from './api';
 import './App.css';
 import  "./nprogress.css";
+import {
+  ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip
+} from 'recharts';
 
 class App extends Component {
   state = {
@@ -51,11 +54,32 @@ class App extends Component {
   }
 
   render() {
+    const { locations, numberOfEvents } = this.state;
     return (
       <div className="App">
-        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
-        <EventList events={this.state.events} showEventCount={this.state.showEventCount} />
-        <NumberOfEvents updateEventCount={this.updateEventCount} />
+        <h1>Meet App</h1>
+        <h4>Choose your nearest city</h4>
+        <CitySearch updateEvents={this.updateEvents} locations={locations} />
+        <NumberOfEvents
+          updateEvents={this.updateEvents}
+          numberOfEvents={numberOfEvents}
+        />       
+         <h4>Events in each city</h4>
+
+         <ScatterChart
+          width={400}
+          height={400}
+          margin={{
+            top: 20, right: 20, bottom: 20, left: 20,
+          }}
+        >
+          <CartesianGrid />
+          <XAxis type="number" dataKey="x" name="stature" unit="cm" />
+          <YAxis type="number" dataKey="y" name="weight" unit="kg" />
+          <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+          <Scatter name="A school" data={data} fill="#8884d8" />
+        </ScatterChart>
+
       </div>
     );
   }
