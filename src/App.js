8cd5fd1432ff5e9fc95b7189ad259d8694175bd4
Doug Rush <71516868+rushdm15@users.noreply.github.com>
2021-01-31 18:43:56 +0200

    edit

--- src/App.js
+++ src/App.js
@@ -73,11 +73,11 @@ class App extends Component {
 };
 
 getData = () => {
-  const {locations, events} = this.state;
+  const { locations, events } = this.state;
   const data = locations.map((location)=>{
     const number = events.filter((event) => event.location === location).length
     const city = location.split(' ').shift()
-    return {city, number};
+    return { city, number };
   })
   return data;
 };
