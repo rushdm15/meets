027ea926681f1d7265a9915aed0f4365c69aebec
Doug Rush <71516868+rushdm15@users.noreply.github.com>
2021-02-26 21:04:31 +0200

    edit errors

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -15,12 +15,6 @@ const EventGenre = ({ events }) => {
     });
     return data;
     };
-    // const data = [
-    //     { name: 'Group A', value: 400 },
-    //     { name: 'Group B', value: 300 },
-    //     { name: 'Group C', value: 300 },
-    //     { name: 'Group D', value: 200 },
-    // ];
 
 // eslint-disable-next-line 
     const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
